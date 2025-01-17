baa9c07bea1d35691886eff9f670191752a28224
steiner <steiner3044@163.com>
2025-01-17 10:51:42 +0800

    misc: project setting for alias of `@`, and title of index, test in backend

--- vue-frontend/vite.config.ts
+++ vue-frontend/vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from "vite-plugin-vue-devtools"
+import { fileURLToPath, URL} from "node:url"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    vueDevTools({
+      launchEditor: "code"
+    })
+  ],
 
+  resolve: {
+   /*  alias: { 
+      find: '@', 
+      replacement: fileURLToPath(new URL('./src', import.meta.url)) 
+    } */
+
+      alias: {
+        "@": fileURLToPath(new URL('./src', import.meta.url))
+      }
+  },
+  
   server: {
     watch: {
       usePolling: true,
